e9664e5411203b9e797d03714cacd43b06ece576
Arthals <zhuozhiyongde@126.com>
2025-04-19 01:00:51 +0800

    📝 docs: Template

--- Cheatsheet/compile.sh
+++ Cheatsheet/compile.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
+# 确保生成Cheatsheet-All.md文件先
 cat source/Cheatsheet-0*.md > source/Cheatsheet-All.md
 
-# to tex
+rm -f cheatsheet.tex cheatsheet.pdf
+
+# 生成tex文件
 pandoc "source/Cheatsheet-All.md" \
     --from markdown-simple_tables-multiline_tables-pipe_tables \
     -o cheatsheet.tex \
@@ -11,16 +16,9 @@ pandoc "source/Cheatsheet-All.md" \
     -B template/before_body.tex \
     -A template/after_body.tex
 
-# to pdf
-pandoc "source/Cheatsheet-All.md" \
-    --from markdown-simple_tables-multiline_tables-pipe_tables \
-    -o cheatsheet.pdf \
-    --pdf-engine=xelatex \
-    --variable=documentclass:extarticle \
-    --variable=classoption:8pt \
-    -H template/preamble.tex \
-    -B template/before_body.tex \
-    -A template/after_body.tex \
-    --resource-path=.
+# 直接从tex文件编译PDF (使用xelatex以支持中文)
+xelatex cheatsheet.tex >> /dev/null
+xelatex cheatsheet.tex >> /dev/null  # 运行两次以确保引用和目录正确
 
-rm source/Cheatsheet-All.md
+# 清理临时文件
+rm -f cheatsheet.aux cheatsheet.log cheatsheet.out source/Cheatsheet-All.md
